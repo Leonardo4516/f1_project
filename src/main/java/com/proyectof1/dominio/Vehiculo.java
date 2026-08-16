122e30da15b55594877b94d6014761ae0a7c8f07
Leonardo4516 <lehernan.07@gmail.com>
2026-08-16 11:11:53 -0500

    feat: :sparkles: Crear clase Vehiculo con validacion de velocidad y desgaste de neumaticos.

---
+++ src/main/java/com/proyectof1/dominio/Vehiculo.java
@@ -0,0 +1,72 @@
+package com.proyectof1.dominio;
+
+public class Vehiculo {
+    private String marcaEscuderia;
+    private int velocidadMaxima;
+    private double desgasteNeumaticos;
+
+    
+    public Vehiculo(String marcaEscuderia, int velocidadMaxima, double desgasteNeumaticos) {
+        
+        setMarcaEscuderia(marcaEscuderia);
+        setVelocidadMaxima(velocidadMaxima);
+        setDesgasteNeumaticos(desgasteNeumaticos);
+
+    }
+
+
+    public String getMarcaEscuderia() {
+        return marcaEscuderia;
+    }
+
+
+    public final void setMarcaEscuderia(String marcaEscuderia) {
+
+        if (!(marcaEscuderia == null) && !marcaEscuderia.isEmpty()) {
+
+            this.marcaEscuderia = marcaEscuderia;
+            
+        } else {
+
+            throw new IllegalArgumentException("Argumento inválido, intente de nuevo.");
+
+        }
+    }
+
+
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+
+    public final void setVelocidadMaxima(int velocidadMaxima) {
+        if (velocidadMaxima > 0) {
+
+            this.velocidadMaxima = velocidadMaxima;
+            
+        } else {
+
+            throw new IllegalArgumentException("Argumento inválido, intente de nuevo.");
+
+        }
+    }
+
+
+    public double getDesgasteNeumaticos() {
+        return desgasteNeumaticos;
+    }
+
+
+    public final void setDesgasteNeumaticos(double desgasteNeumaticos) {
+        if (desgasteNeumaticos >= 0.0 && desgasteNeumaticos <= 100.0) {
+
+            this.desgasteNeumaticos = desgasteNeumaticos;
+            
+        } else {
+
+            throw new IllegalArgumentException("Argumento inválido, intente de nuevo.");
+
+        }
+    }
+
+}
