3e1f1917542f1cfa9d2eb8d30ae1b8804b007044
Leonardo4516 <lehernan.07@gmail.com>
2026-08-16 20:13:49 -0500

    feat: :sparkles: Conectar y hacer visible VentanaSimulacion desde el Main.

--- src/main/java/com/proyectof1/Main.java
+++ src/main/java/com/proyectof1/Main.java
@@ -1,7 +1,13 @@
 package com.proyectof1;
 
+import com.proyectof1.infraestructura.adaptadores.entrada.VentanaSimulacion;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        VentanaSimulacion ventana = new VentanaSimulacion();
+
+        ventana.setVisible(true);
+
     }
 }
