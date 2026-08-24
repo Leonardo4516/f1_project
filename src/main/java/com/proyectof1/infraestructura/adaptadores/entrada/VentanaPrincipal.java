f92f0cc11a4456379a4a30b62bddc80f47e8182e
Leonardo4516 <lehernan.07@gmail.com>
2026-08-24 17:48:21 -0500

    feat: mejorar VentanaPrincipal con estilos uniformes de TemaF1
    - Constante SEPARACION_BOTONES = 14 reemplaza numeros sueltos en struts
    - UIManager Button.arc = 10 para bordes redondeados nativos de FlatLaf
    - HOVER_COLOR #333333 mas visible que ASFALTO, pressed usa FONDO
    - Cabecera ya usa TemaF1.titulo() y TemaF1.subtitulo()
    - conBorde() aplicado al panel del menu

--- src/main/java/com/proyectof1/infraestructura/adaptadores/entrada/VentanaPrincipal.java
+++ src/main/java/com/proyectof1/infraestructura/adaptadores/entrada/VentanaPrincipal.java
@@ -1,6 +1,7 @@
 package com.proyectof1.infraestructura.adaptadores.entrada;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
@@ -14,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.Border;
 
 import com.proyectof1.aplicacion.puertos.entrada.CircuitoServicio;
@@ -23,6 +25,8 @@ import com.proyectof1.aplicacion.puertos.salida.RankingRepositorio;
 import com.proyectof1.aplicacion.servicios.SimulacionService;
 
 public class VentanaPrincipal extends JFrame {
+
+    private static final int SEPARACION_BOTONES = 14;
 
     private final CircuitoServicio circuitoServicio;
     private final PilotoServicio pilotoServicio;
@@ -49,6 +53,8 @@ public class VentanaPrincipal extends JFrame {
 
         add(construirCabecera(), BorderLayout.NORTH);
 
+        UIManager.put("Button.arc", 10);
+
         JPanel menu = new JPanel();
         menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
         TemaF1.conBorde(menu);
@@ -63,13 +69,13 @@ public class VentanaPrincipal extends JFrame {
         JButton btnArcade = nuevoBotonMenu("Juego Arcade", "Conduce y esquiva en modo arcade", TemaF1.colorDeEscuderia("Mercedes"), "gamepad");
 
         menu.add(btnCircuitos);
-        menu.add(Box.createVerticalStrut(14));
+        menu.add(Box.createVerticalStrut(SEPARACION_BOTONES));
         menu.add(btnPilotos);
-        menu.add(Box.createVerticalStrut(14));
+        menu.add(Box.createVerticalStrut(SEPARACION_BOTONES));
         menu.add(btnVehiculos);
-        menu.add(Box.createVerticalStrut(14));
+        menu.add(Box.createVerticalStrut(SEPARACION_BOTONES));
         menu.add(btnSimulacion);
-        menu.add(Box.createVerticalStrut(14));
+        menu.add(Box.createVerticalStrut(SEPARACION_BOTONES));
         menu.add(btnArcade);
         add(menu, BorderLayout.CENTER);
 
@@ -132,7 +138,7 @@ public class VentanaPrincipal extends JFrame {
         return cabecera;
     }
 
-    private JButton nuevoBotonMenu(String nombre, String descripcion, java.awt.Color acento, String iconoNombre) {
+    private JButton nuevoBotonMenu(String nombre, String descripcion, Color acento, String iconoNombre) {
         JButton boton = new BotonMenu(nombre, descripcion, acento, iconoNombre);
         boton.setAlignmentX(CENTER_ALIGNMENT);
         boton.setMinimumSize(new Dimension(380, 68));
@@ -143,7 +149,9 @@ public class VentanaPrincipal extends JFrame {
 
     private class BotonMenu extends JButton {
 
-        BotonMenu(String nombre, String descripcion, java.awt.Color acento, String iconoNombre) {
+        private static final Color HOVER_COLOR = new Color(0x333333);
+
+        BotonMenu(String nombre, String descripcion, Color acento, String iconoNombre) {
             super("<html><b style='font-size:14px'>" + nombre.toUpperCase()
                     + "</b><br><font color='#9A9A9A' size='2'>" + descripcion + "</font></html>");
 
@@ -165,7 +173,7 @@ public class VentanaPrincipal extends JFrame {
             addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseEntered(MouseEvent e) {
-                    setBackground(TemaF1.ASFALTO);
+                    setBackground(HOVER_COLOR);
                 }
 
                 @Override
@@ -175,12 +183,12 @@ public class VentanaPrincipal extends JFrame {
 
                 @Override
                 public void mousePressed(MouseEvent e) {
-                    setBackground(TemaF1.BORDE);
+                    setBackground(TemaF1.FONDO);
                 }
 
                 @Override
                 public void mouseReleased(MouseEvent e) {
-                    setBackground(TemaF1.ASFALTO);
+                    setBackground(HOVER_COLOR);
                 }
             });
         }
